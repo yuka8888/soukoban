fd8255e85cfbd34e9770fc95143c1fc2bbcf5aa8
yuka8888 <133742570+yuka8888@users.noreply.github.com>
2024-05-30 13:58:56 +0900

    クリアまで

--- Assets/Scenes/GameManagerScript.cs
+++ Assets/Scenes/GameManagerScript.cs
@@ -1,94 +1,233 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Net;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
 
-    void PrintArray()
+    //void PrintArray()
+    //{
+    //    string debugText = "";
+    //    for (int y = 0; y < map.GetLength(0); y++)
+    //    {
+    //        for (int x = 0; x < map.GetLength(1); x++)
+    //        {
+    //            debugText += map[y, x].ToString() + ", ";
+    //        }
+    //        debugText += "\n";
+    //    }
+    //    Debug.Log(debugText);
+    //}
+    bool IsCleard()
     {
-        string debugText = "";
-        for(int i = 0;i < map.Length; i++)
-        {
-            debugText += map[i].ToString() + ", ";
-        }
-        Debug.Log(debugText);
-    }
+        //Vector2Int型の可変長配列の作成
+        List<Vector2Int> goals = new List<Vector2Int>();
 
-    int GetPlayerIndex()
-    {
-        for(int i = 0; i < map.Length; i++)
+        for (int y = 0; y < map.GetLength(0); y++)
         {
-            if (map[i] == 1)
+            for (int x = 0; x < map.GetLength(1); x++)
             {
-                return i;
+                //格納場所か田舎を判断
+                if (map[y, x] == 3)
+                {
+                    goals.Add(new Vector2Int(x, y));
+                }
             }
         }
-        return -1;
+
+        for (int i = 0; i < goals.Count; i++)
+        {
+            GameObject f = field[goals[i].y, goals[i].x];
+            if (f == null || f.tag != "Box")
+            {
+                //一つでも箱がなかったら条件未達成
+                return false;
+            }
+        }
+        //条件未達成でなければ条件達成
+        return true;
     }
 
-    bool MoveNumber(int number, int moveFrom, int moveTo)
+    Vector2Int GetPlayerIndex()
+    {
+        for (int y = 0; y < field.GetLength(0); y++)
+        {
+            for (int x = 0; x < field.GetLength(1); x++)
+            {
+                //nullだったらタグを調べず次の要素へ移る
+                if (field[y, x] == null) { continue; }
+                //nullだったらcontinueしているので
+                //この行にたどり着く場合はnullでないことが確定
+                //タグの確認を行う
+                if (field[y, x].tag == "Player")
+                {
+                    return new Vector2Int(x, y);
+                }
+            }
+        }
+        return new Vector2Int(-1, -1);
+    }
+
+    bool MoveNumber(Vector2Int moveFrom, Vector2Int moveTo)
     {
         //移動先が範囲外なら移動不可
-        if(moveTo < 0 || moveTo >= map.Length)
+        if (moveTo.y < 0 || moveTo.y >= field.GetLength(0))
+        {
+            return false;
+        }
+        if (moveTo.x < 0 || moveTo.x >= field.GetLength(1))
         {
             return false;
         }
 
-        //移動先に2があったら
-        if (map[moveTo] == 2)
+        if (field[moveTo.y, moveTo.x] != null && field[moveTo.y, moveTo.x].tag == "Box")
         {
-            //どの方向に移動するか
-            int velocity = moveTo - moveFrom;
-
-            //箱の移動
-            bool success = MoveNumber(2, moveTo, moveTo + velocity);
-
-            if (!success)
-            {
-                return false;
-            }
+            Vector2Int velocity = moveTo - moveFrom;
+            bool success = MoveNumber(moveTo, moveTo + velocity);
+            if (!success) { return false; }
         }
 
-        map[moveTo] = number;
-        map[moveFrom] = 0;
+        field[moveTo.y, moveTo.x] = field[moveFrom.y, moveFrom.x];
+        //field[moveFrom.y, moveFrom.x].transform.position = new Vector3(moveTo.x, map.GetLength(0) - moveTo.y, 0);
+        Vector3 moveToPosition = new Vector3(moveTo.x, map.GetLength(0) - moveTo.y, 0);
+
+        field[moveFrom.y, moveFrom.x].GetComponent<Move>().MoveTo(moveToPosition);
+        field[moveFrom.y, moveFrom.x] = null;
+
         return true;
     }
 
+    public GameObject playerPrefab;
+    public GameObject boxPrefab;
+    public GameObject goalPrefab;
+    public GameObject clearText;
+    public GameObject particlePrefab;
+
     //配列の宣言
-    int[] map;
+    int[,] map;
+    GameObject[,] field;//ゲーム管理用
 
     // Start is called before the first frame update
     void Start()
     {
+        clearText.SetActive(false);
+
+        Screen.SetResolution(1280, 720, false);
+
         //配列の実態の作成と初期化
-        map = new int[] { 0, 0, 0, 1, 0, 2, 0, 0, 0 };
-        PrintArray();
+        map = new int[,] {
+            { 0, 0, 0, 0, 0},
+            { 0, 3, 1, 3, 0},
+            { 0, 0, 2, 0, 0},
+            { 0, 2, 3, 2, 0},
+            { 0, 0, 0, 0, 0},
+        };
+
+        field = new GameObject
+            [
+            map.GetLength(0),
+            map.GetLength(1)
+            ];
+
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                if (map[y, x] == 1)
+                {
+                    field[y, x] = Instantiate(
+                      playerPrefab,
+                      new Vector3(x, map.GetLength(0) - y, 0),
+                      Quaternion.identity
+                     );
+                }
+
+                if (map[y, x] == 2)
+                {
+                    field[y, x] = Instantiate(
+                        boxPrefab,
+                        new Vector3(x, map.GetLength(0) - y, 0),
+                        Quaternion.identity);
+                }
+
+                if (map[y, x] == 3)
+                {
+                    field[y, x] = Instantiate(
+                        goalPrefab,
+                        new Vector3(x, map.GetLength(0) - y, 0),
+                        Quaternion.identity);
+                }
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         //右移動
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            int playerIndex = GetPlayerIndex();
+            Vector2Int playerIndex = GetPlayerIndex();
 
-            MoveNumber(1, playerIndex, playerIndex + 1);
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(1, 0));
+            if (IsCleard())
+            {
+                Debug.Log("Clear");
+                clearText.SetActive(true);
 
-            PrintArray();
+                for(int i = 0;i < 3; i++)
+                {
+                     Instantiate(
+                     particlePrefab,
+                     transform.position,
+                     Quaternion.identity
+                       );
+                }
+            }
         }
 
         //左移動
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            int playerIndex = GetPlayerIndex();
+            Vector2Int playerIndex = GetPlayerIndex();
 
-            MoveNumber(1, playerIndex, playerIndex - 1);
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(-1, 0));
+            if (IsCleard())
+            {
+                Debug.Log("Clear");
+                clearText.SetActive(true);
+            }
+        }
 
-            PrintArray();
+        //上移動
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector2Int playerIndex = GetPlayerIndex();
+
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(0, -1));
+            if (IsCleard())
+            {
+                Debug.Log("Clear");
+                clearText.SetActive(true);
+            }
+        }
+
+        //下移動
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector2Int playerIndex = GetPlayerIndex();
+
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(0, 1));
+            if (IsCleard())
+            {
+                Debug.Log("Clear");
+                clearText.SetActive(true);
+            }
         }
 
     }
---
+++ Assets/Scenes/Particle.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    //消滅するまでの時間
+    private float lifeTime;
+    //消滅するまでの残り時間
+    private float leftLifeTime;
+    //移動量
+    private Vector3 velocity;
+    //初期scale
+    private Vector3 defaultScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTime = 0.3f;
+        leftLifeTime = lifeTime;
+        defaultScale = transform.localScale;
+        //ランダムで決まる移動量の最大値
+        float maxVelocity = 5;
+        //各方向へランダムで飛ばす
+        velocity = new Vector3(
+            Random.Range(-maxVelocity, maxVelocity),
+            Random.Range(-maxVelocity, maxVelocity),
+            0
+            );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftLifeTime -= Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+        //残り時間により徐々にScaleを小さくする
+        transform.localScale = Vector3.Lerp(
+            new Vector3(0, 0, 0),
+            defaultScale,
+            leftLifeTime / lifeTime);
+        //残り時間が0以下になったら自身のゲームオブジェクトを消滅
+        if (leftLifeTime <= 0) { Destroy(gameObject); }
+
+    }
+}
